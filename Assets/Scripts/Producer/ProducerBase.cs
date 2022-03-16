180f9719c0370370308147cf4714902b8e573708
balkac <furkan.balkac@mildmania.com>
2022-03-16 10:52:05 +0300

    CU-2dmzrmd - Print out Consumer Implementation

--- Assets/Scripts/Producer/ProducerBase.cs
+++ Assets/Scripts/Producer/ProducerBase.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections;
-using MMFramework_2._0.PhaseSystem.Core.EventListener;
 using UnityEngine;
 
 
 public abstract class ProducerBase : MonoBehaviour
 {
-
 	[SerializeField] protected int _produceLimit;
+
+	[SerializeField] private int _limitPerProduce;
+	
 	[SerializeField] protected float _produceDelayTime;
 
 	[SerializeField] private GameObject _toBeProducedPrefab;
@@ -15,8 +16,8 @@ public abstract class ProducerBase : MonoBehaviour
 	[SerializeField] private Transform _produceTarget;
 
 	[SerializeField] private StorableDropHandler _storableDropHandler;
-	[SerializeField] private StorableFormationController _storableFormationController;
-	
+
+	protected Coroutine _produceRoutine;
 	private int _numberOfProduced;
 	
 	public Action<StorableBase> OnProduced { get; set; }
@@ -24,51 +25,80 @@ public abstract class ProducerBase : MonoBehaviour
 	private void Awake()
 	{
 		_storableDropHandler.OnStorableDropped += OnDropped;
+		OnAwakeCustomActions();
 	}
 
+	private void Start()
+	{
+		OnStartCustomActions();
+	}
+
+	protected virtual void OnStartCustomActions()
+	{
+	}
+
+	protected virtual void OnAwakeCustomActions()
+	{
+		
+	}
+
+	protected virtual void OnDroppedCustomActions()
+	{
+		
+	}
 	private void OnDropped(StorableBase storable)
 	{
 		_numberOfProduced--;
+		OnDroppedCustomActions();
 	}
 
 	private void OnDestroy()
 	{
 		_storableDropHandler.OnStorableDropped -= OnDropped;
+		OnDestroyCustomActions();
 	}
 
-	private void Start()
+	protected virtual void OnDestroyCustomActions()
 	{
-		Produce();
+		
 	}
-	
-	private void Produce()
+	public void StartProduce()
 	{
-		StartCoroutine(ProduceRoutine());
+		_produceRoutine = StartCoroutine(ProduceRoutine());
+	}
+
+	public void StopProduce()
+	{
+		if (_produceRoutine != null)
+		{
+			StopCoroutine(_produceRoutine);
+			_produceRoutine = null;
+		}
 	}
 
 	private IEnumerator ProduceRoutine()
 	{
 		while (true)
 		{
+			yield return new WaitForSeconds(_produceDelayTime);
+			
 			if (_numberOfProduced >= _produceLimit)
 			{
 				yield return null;
 				continue;
 			}
-				
-			var producedObject = Instantiate(_toBeProducedPrefab);
-			producedObject.transform.position = _produceTarget.position;
-			
-			
-			var storable = producedObject.GetComponent<StorableBase>();
 
-			_numberOfProduced++;
-			
-			OnProduced?.Invoke(storable);
-			
-			yield return new WaitForSeconds(_produceDelayTime);
+			for (int i = 0; i < _limitPerProduce; i++)
+			{
+				var producedObject = Instantiate(_toBeProducedPrefab);
+				producedObject.transform.position = _produceTarget.position;
+
+				var storable = producedObject.GetComponent<StorableBase>();
+				_numberOfProduced++;
+				OnProduced?.Invoke(storable);
+				
+				yield return null;
+			}
 		}
 	}
-	
-	
 }
