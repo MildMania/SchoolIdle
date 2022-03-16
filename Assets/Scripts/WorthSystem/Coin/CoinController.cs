c129f96713ab09d9b2f30c36955e1e095d0039c0
balkac <furkan.balkac@mildmania.com>
2022-03-16 18:50:20 +0300

    Money Storable prefab updates

--- Assets/Scripts/WorthSystem/Coin/CoinController.cs
+++ Assets/Scripts/WorthSystem/Coin/CoinController.cs
@@ -1,8 +1,34 @@
+using System;
 using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    public void Collect()
-    {
-    }
+	// [SerializeField] private CoinWorthCollector _coinWorthCollector;
+
+	// [SerializeField] private BaseCollectibleDetector _collectibleDetector;
+	//
+	// public Action<Collectible> OnCollectibleCollected;
+	// private void Awake()
+	// {
+	// 	_collectibleDetector.OnDetected += OnDetected;
+	// }
+	//
+	//
+	// private void OnApplicationQuit()
+	// {
+	// 	_collectibleDetector.OnDetected -= OnDetected;
+	// }
+	//
+	// private void OnDetected(Collectible collectible)
+	// {
+	// 	Collect();
+	// 	Debug.Log("COINCONTROLLER COLLECT");
+	// 	OnCollectibleCollected?.Invoke(collectible);
+	// }
+
+	public void Collect()
+	{
+		Debug.Log("COINCONTROLLER COLLECT");
+		// _coinWorthCollector.CollectWorth(new CoinWorth());
+	}
 }
---
+++ Assets/Scripts/WorthSystem/Coin/WorthCollectedAction/WorthCollectedAction_ParticleEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class WorthCollectedAction_ParticleEffect : WorthCollectedActionBase
+{
+	[SerializeField] private ParticleEffectPlayer _particleEffectPlayer = null;
+
+	public override void WorthCollected<TWorth>(
+		WorthCollector collector, 
+		TWorth worth)
+	{
+		_particleEffectPlayer.Play();
+	}
+}
