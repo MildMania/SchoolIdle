0982348483f82723084eee028f3ed17800d4b403
Okan Tekdas <okan.tekdas@mildmania.com>
2022-03-25 01:39:15 -0700

    loadable/unloadable logic made generic

--- Assets/Scripts/Deliverer/PaperLoadBehaviour.cs
+++ Assets/Scripts/Deliverer/PaperLoadBehaviour.cs
@@ -1,76 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using MMFramework_2._0.PhaseSystem.Core.EventListener;
 using UnityEngine;
 
-public class PaperLoadBehaviour : MonoBehaviour
+public class PaperLoadBehaviour : BaseLoadBehaviour<PaperProducer, Paper>
 {
-    [SerializeField] private UpdatedFormationController _updatedFormationController;
     [SerializeField] private PaperProducerFovController _paperProducerFovController;
-    [SerializeField] private Deliverer _deliverer;
     [SerializeField] private float _loadDelay = .3f;
-
-    private List<PaperProducer> _paperProducers = new List<PaperProducer>();
 
 
     private void Awake()
     {
-        _paperProducerFovController.OnTargetEnteredFieldOfView += OnPaperProducerEnteredFieldOfView;
-        _paperProducerFovController.OnTargetExitedFieldOfView += OnPaperProducerExitedFieldOfView;
+        _paperProducerFovController.OnTargetEnteredFieldOfView += OnProducerEnteredFieldOfView;
+        _paperProducerFovController.OnTargetExitedFieldOfView += OnProducerExitedFieldOfView;
     }
 
 
     private void OnDestroy()
     {
-        _paperProducerFovController.OnTargetEnteredFieldOfView -= OnPaperProducerEnteredFieldOfView;
-        _paperProducerFovController.OnTargetExitedFieldOfView -= OnPaperProducerExitedFieldOfView;
+        _paperProducerFovController.OnTargetEnteredFieldOfView -= OnProducerEnteredFieldOfView;
+        _paperProducerFovController.OnTargetExitedFieldOfView -= OnProducerExitedFieldOfView;
 
 
         StopAllCoroutines();
     }
 
-    private void OnPaperProducerEnteredFieldOfView(PaperProducer paperProducer)
+
+    public override void LoadCustomActions(Paper paper)
     {
-        _paperProducers.Add(paperProducer);
-    }
-
-    private void OnPaperProducerExitedFieldOfView(PaperProducer paperProducer)
-    {
-        _paperProducers.Remove(paperProducer);
-    }
-
-    [PhaseListener(typeof(GamePhase), true)]
-    public void OnGamePhaseStarted()
-    {
-        StartCoroutine(LoadRoutine());
-    }
-
-    private IEnumerator LoadRoutine()
-    {
-        float currentTime = 0;
-
-        while (true)
-        {
-            currentTime += Time.deltaTime;
-
-            if (currentTime > _loadDelay)
-            {
-                if (_paperProducers.Count > 0)
-                {
-                    int index = (int) Random.Range(0, _paperProducers.Count - 0.1f);
-                    Paper paper = null;
-                    if (_paperProducers[index].TryRemoveAndGetLastProducible(ref paper))
-                    {
-                        Transform targetTransform = _updatedFormationController.GetLastTargetTransform(paper.transform);
-                        paper.MoveProducible(targetTransform, _updatedFormationController.Container);
-                        _deliverer.Papers.Add(paper);
-                    }
-                }
-
-                currentTime = 0;
-            }
-
-            yield return null;
-        }
+        Transform targetTransform = _updatedFormationController.GetLastTargetTransform(paper.transform);
+        paper.MoveProducible(targetTransform, _updatedFormationController.Container);
+        _deliverer.Papers.Add(paper);
     }
 }
--- Assets/Scripts/Deliverer/PaperUnloadBehaviour.cs
+++ Assets/Scripts/Deliverer/PaperUnloadBehaviour.cs
@@ -1,16 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using MMFramework_2._0.PhaseSystem.Core.EventListener;
 using UnityEngine;
 
-public class PaperUnloadBehaviour : MonoBehaviour
+public class PaperUnloadBehaviour : BaseUnloadBehaviour<PaperConsumer, Paper>
 {
-    [SerializeField] private UpdatedFormationController _updatedFormationController;
     [SerializeField] private PaperConsumerFovController _paperConsumerFovController;
-    [SerializeField] private Deliverer _deliverer;
     [SerializeField] private float _unloadDelay = .3f;
-
-    private List<PaperConsumer> _paperConsumers = new List<PaperConsumer>();
 
 
     private void Awake()
@@ -31,48 +24,20 @@ public class PaperUnloadBehaviour : MonoBehaviour
 
     private void OnPaperConsumerEnteredFieldOfView(PaperConsumer paperConsumer)
     {
-        _paperConsumers.Add(paperConsumer);
+        _consumers.Add(paperConsumer);
     }
 
     private void OnPaperConsumerExitedFieldOfView(PaperConsumer paperConsumer)
     {
-        _paperConsumers.Remove(paperConsumer);
+        _consumers.Remove(paperConsumer);
     }
 
-    [PhaseListener(typeof(GamePhase), true)]
-    public void OnGamePhaseStarted()
+
+    public override void UnloadCustomActions(int index)
     {
-        StartCoroutine(UnloadRoutine());
-    }
-
-    private IEnumerator UnloadRoutine()
-    {
-        float currentTime = 0;
-
-        while (true)
-        {
-            currentTime += Time.deltaTime;
-
-            if (currentTime > _unloadDelay)
-            {
-                if (_paperConsumers.Count > 0)
-                {
-                    int index = (int) Random.Range(0, _paperConsumers.Count - 0.1f);
-
-                    if (_deliverer.Papers.Count > 0)
-                    {
-                        Paper paper = _deliverer.Papers[_deliverer.Papers.Count - 1];
-                        _deliverer.Papers.Remove(paper);
-                        _updatedFormationController.RemoveAndGetLastTransform();
-                        _paperConsumers[index].Consume(paper);
-                    }
-                    //TODO: Check other unloadable varients (like if money stacked between paper piles)
-                }
-
-                currentTime = 0;
-            }
-
-            yield return null;
-        }
+        Paper paper = _deliverer.Papers[_deliverer.Papers.Count - 1];
+        _deliverer.Papers.Remove(paper);
+        _updatedFormationController.RemoveAndGetLastTransform();
+        _consumers[index].Consume(paper);
     }
 }
