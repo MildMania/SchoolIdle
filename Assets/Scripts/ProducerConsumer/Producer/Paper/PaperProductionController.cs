cc2755d8f539d03021d2ab3297f38c2157082c24
balkac <furkan.balkac@mildmania.com>
2022-03-29 12:20:31 +0300

    Classroom Upgrade UI added to scene

--- Assets/Scripts/ProducerConsumer/Producer/Paper/PaperProductionController.cs
+++ Assets/Scripts/ProducerConsumer/Producer/Paper/PaperProductionController.cs
@@ -1,9 +1,9 @@
+using System;
 using MMFramework_2._0.PhaseSystem.Core.EventListener;
 
 public class PaperProductionController : ProductionController<PaperProducer, Paper>
 {
-	[PhaseListener(typeof(GamePhase), true)]
-	public void OnGamePhaseStarted()
+	private void OnEnable()
 	{
 		StartCoroutine(ProduceRoutine(_resource));
 	}
