9cfc9b303fdd93c281ae43675a2076f665ca684d
balkac <furkan.balkac@mildmania.com>
2022-03-30 10:14:34 +0300

    CU-28t8djh - Unlockable Library completed

--- Assets/Scripts/ProducerConsumer/Producer/Folder/FolderProductionController.cs
+++ Assets/Scripts/ProducerConsumer/Producer/Folder/FolderProductionController.cs
@@ -1,10 +1,16 @@
 ﻿
+using System;
 using MMFramework_2._0.PhaseSystem.Core.EventListener;
 
 public class FolderProductionController : ProductionController<FolderProducer, Folder>
 {
-	[PhaseListener(typeof(GamePhase), true)]
-	public void OnGamePhaseStarted()
+	// [PhaseListener(typeof(GamePhase), true)]
+	// public void OnGamePhaseStarted()
+	// {
+	// 	StartCoroutine(ProduceRoutine(_resource));
+	// }
+
+	private void OnEnable()
 	{
 		StartCoroutine(ProduceRoutine(_resource));
 	}
